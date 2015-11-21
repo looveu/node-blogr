06e4974764bf6336deaa24dcfc5f666a45726534
dom <dom@gameand.me>
2015-11-21 18:33:51 +0800

    comment no use script loader

--- public/admin__/scripts/app.js
+++ public/admin__/scripts/app.js
@@ -107,10 +107,10 @@ angular
               name:'sbAdminApp',
               files:[
               'scripts/controllers/main.js',
-              'scripts/directives/timeline/timeline.js',
-              'scripts/directives/notifications/notifications.js',
-              'scripts/directives/chat/chat.js',
-              'scripts/directives/dashboard/stats/stats.js'
+              'scripts/directives/timeline/timeline.js'
+              // 'scripts/directives/notifications/notifications.js',
+              // 'scripts/directives/chat/chat.js',
+              // 'scripts/directives/dashboard/stats/stats.js'
               ]
             })
           }
